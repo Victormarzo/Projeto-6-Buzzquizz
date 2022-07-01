1a7c63f746047c5f091381658a59ba06b25960c8
Lucas Tavares <l.tavaresc@hotmail.com>
2022-07-01 00:21:00 -0300

    Criação de níveos do quizz adicionada.

--- pagina3.js
+++ pagina3.js
@@ -3,7 +3,8 @@ let quizzCriado = false
 let definicoesQuizz = []
 let qntPerg = null
 let perguntas = []
-let  qntNiveis = 0
+let niveis = []
+let qntNiveis = 0
 //Pagina 3.1
 function abrePagina3() {
     pagina.innerHTML =
@@ -14,13 +15,14 @@ function abrePagina3() {
                 <div>
                     <input placeholder="Título do seu quizz" type="text">
                     <input placeholder="URL da imagem do seu quizz" type="text">
-                    <input placeholder="Quantidade de perguntas do quizz" type="text">
-                    <input placeholder="Quantidade de níveis do quizz" type="text">
+                    <input placeholder="Quantidade de perguntas do quizz" type="number">
+                    <input placeholder="Quantidade de níveis do quizz" type="number">
                 </div>
             </div>
             <div onclick="guardaInputs()" class="button">Prosseguir para criar perguntas</div>
         </div>
     `
+    scrollTop();
 }
 function guardaInputs() {
     let titulo, url
@@ -124,6 +126,7 @@ function crieSuasPerguntas(titulo, url) {
     //Depois de criar o HTML, abre a primeira gaveta:
     document.querySelector('.gaveta-titulo').classList.add('hidden')
     document.querySelector('.gaveta-perguntas').classList.remove('hidden')
+    scrollTop();
 }
 
 function abreGaveta(caixa, caixaFechada) {
@@ -169,34 +172,25 @@ function verificaRequisitosPerguntas() {
     let tudoNosConformes = 0
     //Verifica requisitos:
     perguntas.forEach((item, indice) => {
+        let isHexadecimal = verificaHexadecimial(item.corDeFundoDaPergunta)
 
         if ((item.textoDaPergunta).length < 20 && item.textoDaPergunta !== "") {
             alert('O texto das perguntas devem ter no mínimo 20 caracteres. ')
         }
-        if (item.corDeFundoDaPergunta !== "") {
-            let isHexadecimal = verificaHexadecimial(item.corDeFundoDaPergunta)
-            if (((item.corDeFundoDaPergunta).length !== 7 || item.corDeFundoDaPergunta[0] !== "#" || isHexadecimal === false))
-                alert('Cor de fundo: deve ser uma cor em hexadecimal (começar em "#", seguida de 6 caracteres hexadecimais, ou seja, números ou letras de A a F)')
+        if ((item.corDeFundoDaPergunta !== "") && ((item.corDeFundoDaPergunta).length !== 7 || item.corDeFundoDaPergunta[0] !== "#" || isHexadecimal === false)) {
+            alert('Cor de fundo: deve ser uma cor em hexadecimal (começar em "#", seguida de 6 caracteres hexadecimais, ou seja, números ou letras de A a F)')
         }
-        if (item.urlDaImagem !== "") {
-            if (isURL(item.urlDaImagem) === false) {
-                alert(`O link da imagem da resposta correta na pergunta ${indice + 1} não é válido.`)
-            }
+        if ((item.urlDaImagem !== "") && (isURL(item.urlDaImagem) === false)) {
+            alert(`O link da imagem da resposta correta na pergunta ${indice + 1} não é válido.`)
         }
-        if (item.urlDaImagem1 !== "") {
-            if (isURL(item.urlDaImagem1) === false) {
-                alert(`O link da imagem da resposta incorreta(1) na pergunta ${indice + 1} não é válido.`)
-            }
+        if ((item.urlDaImagem1 !== "") && (isURL(item.urlDaImagem1) === false)) {
+            alert(`O link da imagem da resposta incorreta(1) na pergunta ${indice + 1} não é válido.`)
         }
-        if (item.urlDaImagem2 !== "") {
-            if (isURL(item.urlDaImagem2) === false) {
-                alert(`O link da imagem da resposta incorreta(2) na pergunta ${indice + 1} não é válido.`)
-            }
+        if ((item.urlDaImagem2 !== "") && (isURL(item.urlDaImagem2) === false)) {
+            alert(`O link da imagem da resposta incorreta(2) na pergunta ${indice + 1} não é válido.`)
         }
-        if (item.urlDaImagem3 !== "") {
-            if (isURL(item.urlDaImagem3) === false) {
-                alert(`O link da imagem da resposta incorreta(3) na pergunta ${indice + 1} não é válido.`)
-            }
+        if ((item.urlDaImagem3 !== "") && (isURL(item.urlDaImagem3) === false)) {
+            alert(`O link da imagem da resposta incorreta(3) na pergunta ${indice + 1} não é válido.`)
         }
         else {
             // adiciona +1 pra cada caixa de pergunta preenchida corretamente
@@ -215,19 +209,18 @@ function verificaRequisitosPerguntas() {
 function verificaAntesPostagem() {
     let tudoNosConformes = 0
     perguntas.forEach((item, indice) => {
-        if (item.textoDaPergunta !== ""){
-           if(item.respostaCorreta === "" || item.respostaIncorreta1 === "") {
+        if ((item.textoDaPergunta !== "") && (item.respostaCorreta === "" || item.respostaIncorreta1 === "")) {
             alert(`Erro na pergunta(${indice + 1}): Toda pergunta deve ter ao menos uma resposta correta e uma incorreta.`)
-           }
-           //Se toda resposta precisar de uma imagem, adicionar a verificação aqui.
+            //Se toda resposta precisar de uma imagem, adicionar a verificação aqui.
         }
         else {
             tudoNosConformes++
         }
     })
-    if(tudoNosConformes == qntPerg){
+    if (tudoNosConformes == qntPerg) {
         crieSeusNiveis();
     }
+    else ('Algo deu errado...')
 }
 
 function verificaHexadecimial(cor) {
@@ -257,27 +250,93 @@ function isURL(url) {
 
 //Pagina 3.3
 
-function crieSeusNiveis(){
-pagina.html = 
-`
-<div class="pagina3">
-            <h1>Agora, decida os níveis</h1>
-            <div onclick="abreGaveta(this, 'gaveta-niveis')" class="caixa-nivel Nivel${i}">
-                <div class="gaveta-titulo">
-                    <h1>Nivel ${i}</h1>
-                    <img src="img/createicon.svg">
-                </div>
-                <div class="gaveta-niveis hidden">
-                    <div>
-                        <h1>Nivel ${i}</h1>
-                        <input placeholder="Título do nível" type="text">
-                        <input placeholder="% de acerto mínima" type="text">
-                        <input placeholder="URL da imagem do nível" type="text">
-                        <input placeholder="Descrição do nível" type="text">
-                    </div>
-                </div>
-            </div>
-            <div onclick="recebeNiveis()" class="button">Prosseguir para criar níveis</div>
+function crieSeusNiveis() {
+    let niveisHTML = ''
+    for (let i = 1; i <= qntNiveis; i++) {
+        niveisHTML +=
+            `
+<div onclick="abreGaveta(this, 'gaveta-niveis')" class="caixa-nivel nivel${i}">
+    <div class="gaveta-titulo">
+        <h1>Nivel ${i}</h1>
+        <img src="img/createicon.svg">
+    </div>
+    <div class="gaveta-niveis hidden">
+        <div>
+            <h1>Nivel ${i}</h1>
+            <input placeholder="Título do nível" type="text">
+            <input placeholder="% de acerto mínima" type="number">
+            <input placeholder="URL da imagem do nível" type="text">
+            <input placeholder="Descrição do nível" type="text">
         </div>
+    </div>
+</div>
 `
+    }
+    pagina.innerHTML =
+        `
+<div class="pagina3">
+    <h1>Agora, decida os níveis</h1>
+    ${niveisHTML}
+    <div onclick="recebeNiveis()" class="button">Prosseguir para criar níveis</div>
+</div>
+`
+    document.querySelector('.gaveta-titulo').classList.add('hidden')
+    document.querySelector('.gaveta-niveis').classList.remove('hidden')
+    scrollTop();
+}
+function scrollTop() {
+    document.querySelector('h1').scrollIntoView()
+}
+function recebeNiveis() {
+    niveis = []
+    let dataInputs = []
+
+    for (i = 1; i <= qntNiveis; i++) {
+        let nivel = document.querySelector(`.nivel${i}`);
+        let inputsList = nivel.querySelectorAll('input');
+        //salva em dataPerg apenas as informações de cada caixa de perguntas:
+        inputsList.forEach(input => { dataInputs.push(input.value) });
+        //coloca dento do perguntas um objeto com as informações do array anterior, para facilitar a manipulação:
+        niveis.push({
+            tituloDoNivel: dataInputs[0],
+            acertoMinimo: dataInputs[1],
+            urlDaImagem: dataInputs[2],
+            descNivel: dataInputs[3]
+        });
+        // limpa inputsList para que ao voltar no inicio do loop, fiquem apenas as informações da próxima caixa de pergunta.
+        dataInputs = [];
+    }
+    verificaNiveis()
+}
+
+function verificaNiveis() {
+    let umNivel0 = false
+    let tudoNosConformes = 0
+    niveis.forEach((item, index) => {
+        if (item.tituloDoNivel !== "" && item.tituloDoNivel.length < 10) {
+            alert(`Erro no nivel(${index + 1}): Título do nível deve ter no mínimo 10 caracteres. `)
+        }
+        if (item.acertoMinimo !== "" && (item.acertoMinimo > 100 || item.acertoMinimo < 0)) {
+            alert(`Erro no nivel(${index + 1}): O numero deve estar entre 0 e 100. `)
+        }
+        if (item.urlDaImagem !== "" && (isURL(item.urlDaImagem) === false)){
+            alert(`Erro no nivel(${index + 1}): Deve inserir um URL válido. `)
+        }
+        if (item.descNivel !== "" && item.descNivel.length < 30){
+            alert(`Erro no nivel(${index + 1}): A descrição deve ter mais que 30 caracteres. `)
+        }
+        if (item.acertoMinimo !== "" && (item.acertoMinimo == 0)){
+            tudoNosConformes++
+            umNivel0 = true
+        }
+        else {tudoNosConformes++}
+    })
+    console.log(umNivel0)
+    if(umNivel0 === false){
+        alert('Deve haver ao menos um nivel com 0% de acerto mínimo.')
+    }
+    if(tudoNosConformes === qntNiveis && umNivel0 === true){
+        alert('tudo certo')
+    }
+    //duvida: sera que deve ser necessario colocar imagem?
 }
